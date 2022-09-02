d7c372e30acef91926117e30aee0919015caec26
lasere77 <colin.mewen@gmail.com>
2022-09-02 11:29:52 +0200

    bug semi solved

--- src/fr/lasere/loggepy/Passwords/DelPassword.java
+++ src/fr/lasere/loggepy/Passwords/DelPassword.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 import fr.lasere.loggepy.Log.LogWriting;
 
@@ -16,15 +17,11 @@ public class DelPassword {
 	private String allPassword;
 	private String result;
 	
-	/*
-	 * You still need to remove unnecessary line breaks.
-	 */
 	public String getDelPassword(String namePassword, String password, String oldPassword) throws IOException {
 		if(namePassword == "" || password == "") {
 			return "please put argument";
 		}
 		result = namePassword + "=" + password;
-		fullPassword = "\n" + namePassword + "=" + password;
 		if(!passwordFile.exists()) {
 			try {
 				passwordFile.createNewFile();
@@ -32,6 +29,13 @@ public class DelPassword {
 				lw.WriteLogFatal("the password file did not succeed in this creation please report the bug thank you for your understanding");
 				return "you don't have a password...";
 			}
+		}
+		//String str1 = new Boolean(lastLine(namePassword, password)).toString();
+		//lw.WriteLogInfo(str1);
+		if(lastLine(namePassword, password)) {
+			fullPassword = "\n" + namePassword + "=" + password + "\n";
+		}else {
+			fullPassword = "\n" + namePassword + "=" + password;
 		}
 		FileWriter fw = new FileWriter(passwordFile);
 		BufferedWriter bw = new BufferedWriter(fw);
@@ -44,5 +48,23 @@ public class DelPassword {
 	private String delPassword(String fullPassword, String oldPassword) throws IOException {
 		allPassword = oldPassword.replaceAll(fullPassword, "");
 		return allPassword;
+	}
+	
+	@SuppressWarnings("resource")
+	private boolean lastLine(String namePassword, String password) throws IOException {
+		String path = "C:\\Program Files (x86)\\loggepy-edition-java\\password\\passwords";
+		File file = new File(path);
+		Scanner sc = new Scanner(file);
+		String line = "";
+		while(sc.hasNextLine()) {
+			line = sc.nextLine();
+		}
+		lw.WriteLogInfo(line);
+		String temp = namePassword + "=" + password;
+		//Why does this condition not work?
+		if(line == temp || line == "\n" + temp || line == temp + "\n") {
+			return true;
+		}
+		return false;
 	}
 }
