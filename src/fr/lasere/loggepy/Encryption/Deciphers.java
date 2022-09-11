c87a68f07143d7e05719e9e387ab2aeedca25530
lasere77 <colin.mewen@gmail.com>
2022-09-11 21:46:08 +0200

    adds lag gold reading/writing of upper and lower case letters and numbers (unstable/errorgit statusgit status)

--- src/fr/lasere/loggepy/Encryption/Deciphers.java
+++ src/fr/lasere/loggepy/Encryption/Deciphers.java
@@ -1,6 +1,11 @@
 package fr.lasere.loggepy.Encryption;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import fr.lasere.loggepy.Log.LogWriting;
 import javafx.scene.control.Label;
@@ -8,21 +13,64 @@ import javafx.scene.control.Label;
 public class Deciphers {
 	
 	private LogWriting lw = new LogWriting();
-	@SuppressWarnings("unused")
+	private final Path passwordFile = Paths.get("C:\\loggepy-edition-java\\password\\passwords");
+	private String currentpasswordLine = "";
+	private String PasswordLineEncrypt = "";
+	private String PasswordEncrypt;
 	private int MainPassword;
 	
 	public boolean decipher(String iDEntryMainPassword, Label IDLabelErrorMainPassword) throws IOException {
 		System.out.println(iDEntryMainPassword);
 		lw.WriteLogInfo("master password processing...");
 		try {
-			@SuppressWarnings("unused")
-			int MainPassword = Integer.parseInt(iDEntryMainPassword);
+			MainPassword = Integer.parseInt(iDEntryMainPassword);
+			System.out.println(MainPassword);
 		} catch (Exception e) {
 			lw.WriteLogError("the user did not enter something correct...");
 			IDLabelErrorMainPassword.setText("please put only numbers");
 			return false;
 		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\loggepy-edition-java\\password\\passwords"), "UTF-8"));
+		String line = br.readLine();
+		while (line != null) {
+			for(int i = 0; i != line.length(); i++) {
+				System.out.println("origine = " + line.charAt(i));
+				cesar(MainPassword, line.charAt(i));
+			}
+			System.out.println(line);
+			line = br.readLine();
+		}
+		br.close();
 		return true;
 	}
 	
+	private char cesar(int nb, char input) {
+		char output = '2';
+		while (nb < 26) {
+			nb -= 26;
+		}
+		for(int i = 0; i != EncryptionArguments.lengthUppercase; i++) {
+			if (input == EncryptionArguments.uppercase[i]) {
+				try {
+					output = EncryptionArguments.uppercase[i + nb];
+				} catch (Exception e) {
+					output = EncryptionArguments.uppercase[nb];
+				}
+			}else if (input == EncryptionArguments.lowercase[i]) {
+				try {
+					output = EncryptionArguments.lowercase[i + nb];
+				} catch (Exception e) {
+					output = EncryptionArguments.lowercase[nb];
+				}
+			}else if (input == EncryptionArguments.nb[i]) {
+				try {
+					output = (char) EncryptionArguments.nb[i + nb];
+				} catch (Exception e) {
+					output = (char) EncryptionArguments.nb[nb];
+				}
+			}
+		}
+		System.out.println(output);
+		return output;
+	}
 }
--- src/fr/lasere/loggepy/Encryption/EncryptionArguments.java
+++ src/fr/lasere/loggepy/Encryption/EncryptionArguments.java
@@ -3,10 +3,14 @@ package fr.lasere.loggepy.Encryption;
 public class EncryptionArguments {
 	
 	//password aruments
-	protected static String[] uppercase = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-	protected static String[] lowercase = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-	protected static String[] nb = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	protected static char[] uppercase = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	protected static char[] lowercase = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	protected static int[] nb = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	//do not put its it's argument "? ² \ { }" in the list
 	protected static String[] specialCharacter = {"\"", "@", "<", "#", "&", ">", "~", "!", "-", "'", "_"};
 	
+	//EncryptionArguments
+	protected final static int lengthUppercase = uppercase.length;
+	protected final static int lengthLowercase = lowercase.length;
+	protected final static int lengthNb = nb.length;
 }
